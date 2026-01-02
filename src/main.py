06ffca4d1b5fb20b83b244d9d504fe44e827eeb8
fuadnafiz98 <fuadnafiz98@gmail.com>
2026-01-03 00:31:05 +0600

    chore: basic python setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-""" """
+import psycopg
+from psycopg.rows import dict_row
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.getLogger("psycopg").setLevel(logging.DEBUG)
+
+
+POSTGRESQL_CONN_STR = "postgresql://postgres:secure_password@localhost:5432/local_db"
+
+with psycopg.connect(conninfo=POSTGRESQL_CONN_STR, row_factory=dict_row) as conn:
+    cur = conn.execute("""
+            CREATE TABLE IF NOT EXISTS test (
+                id serial PRIMARY KEY, 
+                counter integer
+            )
+        """)
+
+    record = conn.execute("""
+            SELECT * from test
+        """).fetchone()
+    print(record)
+    conn.commit()
